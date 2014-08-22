ce6b4b3b7c24844f0be589e8e42df45f65d2dae8
pratikmallya <pratik.mallya@gmail.com>
2014-08-22 13:05:10 -0500

    ADD: better repr for the model objects

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -14,25 +14,34 @@ class QCModelTest(TestCase):
         tz = timezone.get_default_timezone()
         question = Question()
         question.question_text = 'Who is the most famous actress?'
-        question.pub_date = datetime.datetime(2012, 3, 31, 11, 30, 5, 182371, tzinfo=tz)
+        question.pub_date = datetime.datetime(
+            2012, 3, 31, 11, 30, 5, 182371, tzinfo=tz
+        )
         question.save()
 
         saved_questions = Question.objects.all()
         self.assertEqual(saved_questions.count(), 1)
         saved_question = saved_questions[0]
-        self.assertEqual(saved_question.question_text, 'Who is the most famous actress?')
+        self.assertIn('Who is the most famous actress?',
+            repr(saved_question),
+        )
+        self.assertEqual(saved_question.question_text,
+            'Who is the most famous actress?')
         self.assertEqual(saved_question.pub_date,
             datetime.datetime(2012, 3, 31, 11, 30, 5, 182371, tzinfo=tz))
 
         choice = Choice()
-        choice.text = "1. Demi Moore 2. Julia Roberts"
+        choice.choice_text = '1. Demi Moore 2. Julia Roberts'
         choice.votes = 1000
         choice.save()
 
         saved_choices = Choice.objects.all()
         self.assertEqual(saved_choices.count(), 1)
         saved_choice = saved_choices[0]
-        self.assertEqual(saved_choice.text, "1. Demi Moore 2. Julia Roberts")
+        self.assertIn('1. Demi Moore 2. Julia Roberts',
+            repr(saved_choice),
+        )
+        self.assertEqual(saved_choice.choice_text, '1. Demi Moore 2. Julia Roberts')
         self.assertEqual(saved_choice.votes, 1000)
 
 
